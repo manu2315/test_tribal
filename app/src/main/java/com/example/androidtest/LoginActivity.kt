ff306741236805eac8e201718217ed3287f065c1
manu2315 <manuairalert@gmail.com>
2020-08-26 15:40:37 -0500

    [INIT] Init of git repo

---
+++ app/src/main/java/com/example/androidtest/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.example.androidtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
