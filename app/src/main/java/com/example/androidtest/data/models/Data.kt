414ed712488a339fa5cd7ae1db60e06e145cd9d7
manu2315 <manuairalert@gmail.com>
2020-08-27 18:28:51 -0500

    [ADDED] Model of response added

---
+++ app/src/main/java/com/example/androidtest/data/models/Data.kt
@@ -0,0 +1,5 @@
+package com.example.androidtest.data.models
+
+data class Data(
+    val file: String
+)
---
+++ app/src/main/java/com/example/androidtest/data/models/ServiceResponse.kt
@@ -0,0 +1,7 @@
+package com.example.androidtest.data.models
+
+data class ServiceResponse(
+    val code: Int,
+    val data: Data,
+    val success: Boolean
+)
