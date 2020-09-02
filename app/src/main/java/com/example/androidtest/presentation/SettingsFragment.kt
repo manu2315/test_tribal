827d4acc405cb2b2f3004321c8403699a352445d
manu2315 <manuairalert@gmail.com>
2020-09-02 16:50:21 -0500

    [ADDED] Settings layout info added

--- app/src/main/java/com/example/androidtest/presentation/SettingsFragment.kt
+++ app/src/main/java/com/example/androidtest/presentation/SettingsFragment.kt
@@ -5,12 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.example.androidtest.R
+import com.example.androidtest.data.objects.UserLogged
+import com.example.androidtest.databinding.FragmentSettingsBinding
+import com.squareup.picasso.Picasso
 
 
 class SettingsFragment : Fragment() {
 
 
+    private val binding:FragmentSettingsBinding by lazy {
+        FragmentSettingsBinding.inflate(
+            LayoutInflater.from(context),null,false
+        )
+            //.apply {  }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -18,9 +28,17 @@ class SettingsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    ): View? = binding.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner=this.viewLifecycleOwner
+        setupSettings()
     }
 
+    private fun setupSettings(){
+        val user= UserLogged
+        binding.user=user.getUser()
+        Picasso.get().load(user.getUser()?.photoUrl!!).into(binding.profileImage)
+    }
 }
